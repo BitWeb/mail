0a1f73518991c88a1caeb2077cdaa67ab7c9b474
Rain Ramm <rammrain@gmail.com>
2014-04-15 19:15:23 +0300

    Add support for events.

--- src/BitWeb/Mail/Service/MailService.php
+++ src/BitWeb/Mail/Service/MailService.php
@@ -3,6 +3,8 @@
 namespace BitWeb\Mail\Service;
 
 use BitWeb\Mail\Configuration;
+use Zend\EventManager\EventInterface;
+use Zend\EventManager\EventManagerInterface;
 use Zend\Mail\Message;
 use Zend\Mail\Transport\TransportInterface;
 use Zend\Mime\Mime;
@@ -10,6 +12,7 @@ use Zend\Mime\Part;
 
 class MailService
 {
+    const SEND_MAIL = 'bitweb.mailService.send';
 
     /**
      * @var TransportInterface
@@ -20,11 +23,29 @@ class MailService
      * @var Configuration
      */
     protected $configuration;
+
+    /**
+     * @var EventManagerInterface
+     */
+    protected $eventManager;
     protected $bypassConfiguration = false;
 
-    public function __construct(TransportInterface $transportInterface, Configuration $configuration)
+    /**
+     * @param \Zend\EventManager\EventManagerInterface $eventManager
+     * @return self
+     */
+    public function setEventManager($eventManager)
     {
-        $this->setTransport($transportInterface);
+        $this->eventManager = $eventManager;
+        return $this;
+    }
+
+    /**
+     * @return \Zend\EventManager\EventManagerInterface
+     */
+    public function getEventManager()
+    {
+        return $this->eventManager;
     }
 
     /**
@@ -53,6 +74,70 @@ class MailService
     public function setBypassConfiguration($bypass = false)
     {
         $this->bypassConfiguration = (bool)$bypass;
+    }
+
+    public function __construct(TransportInterface $transportInterface, Configuration $configuration)
+    {
+        $this->setTransport($transportInterface);
+        $this->setConfiguration($configuration);
+    }
+
+    public function initializeListener()
+    {
+        if ($this->eventManager !== null) {
+            $this->getEventManager()->attach(self::SEND_MAIL, function (EventInterface $e) use ($this) {
+                $target = $e->getTarget();
+                $message = new Message();
+                $attachments = [];
+                if (isset($target['to']) && is_array($target['to'])) {
+                    $message->setTo($target['cc']);
+                }
+
+                if (isset($target['cc']) && is_array($target['cc'])) {
+                    foreach ($target['cc'] as $cc) {
+                        $message->addCc($cc);
+                    }
+                }
+
+                if (isset($target['bcc']) && is_array($target['bcc'])) {
+                    foreach ($target['bcc'] as $bcc) {
+                        $message->addBcc($bcc);
+                    }
+                }
+
+                if (isset($target['from']) && is_array($target['from'])) {
+                    $message->setFrom($target['from']);
+                }
+
+                if (isset($target['subject']) && is_array($target['subject'])) {
+                    $message->setSubject($target['subject']);
+                }
+
+                if (isset($target['body']) && is_array($target['body'])) {
+                    $message->setBody($target['body']);
+                }
+
+                if (isset($target['attachments']) && is_array($target['attachments'])) {
+                    foreach ($target['attachments'] as $filePath) {
+                        $attachments[] = $this->getPartFromFile($filePath);
+                    }
+                }
+
+                $this->send($message, $attachments);
+            });
+        }
+    }
+
+    protected function getPartFromFile($filePath)
+    {
+        $part = new Part(file_get_contents($filePath));
+        $part->type = mime_content_type($filePath);
+        $part->disposition = \Zend\Mime\Mime::DISPOSITION_ATTACHMENT;
+        $part->encoding = \Zend\Mime\Mime::ENCODING_BASE64;
+        $part->charset = 'UTF-8';
+        $part->filename = basename($filePath);
+
+        return $part;
     }
 
     public function send(Message $message, array $attachments = array())
